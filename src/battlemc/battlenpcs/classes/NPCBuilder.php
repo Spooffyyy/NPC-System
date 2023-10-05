76000775edbc8c2c3c6ea45e5a3c41281adad2f7
CherryBurst103 <145829403+CherryBurst103@users.noreply.github.com>
2023-10-05 05:02:04 -0700

    Update NPCBuilder.php

--- src/battlemc/battlenpcs/classes/NPCBuilder.php
+++ src/battlemc/battlenpcs/classes/NPCBuilder.php
@@ -4,162 +4,113 @@ namespace battlemc\battlenpcs\classes;
 
 use battlemc\battlenpcs\entities\CustomNPC;
 use battlemc\battlenpcs\handler\NPCEventHandler;
-use pocketmine\entity\Entity;
 use pocketmine\entity\Skin;
 use pocketmine\world\World;
 use pocketmine\math\Vector3;
 
 use InvalidArgumentException;
 use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\StringTag;
+use pocketmine\nbt\tag\ByteArrayTag;
 
 class NPCBuilder
 {
+    private $name;
+    private $tags = [];
+    private $type;
+    private $position;
+    private $world;
+    private $handler = null;
 
-	/**
-	 * @var string
-	 */
-	private $name;
+    public function setType(CustomType $type): self
+    {
+        $this->type = $type;
+        return $this;
+    }
 
-	/**
-	 * @var AssignableTag[]
-	 */
-	private $tags = [];
+    public function addTag(AssignableTag $tag): self
+    {
+        $this->tags[] = $tag;
+        return $this;
+    }
 
-	/**
-	 * @var CustomType
-	 */
-	private $type;
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
 
-	/**
-	 * @var Vector3
-	 */
-	private $position;
+    public function setWorld(World $world): self
+    {
+        $this->world = $world;
+        return $this;
+    }
 
-	/** @var World */
-	private $world;
+    public function setPosition(Vector3 $position): self
+    {
+        $this->position = $position;
+        return $this;
+    }
 
+    public function setHandler(NPCEventHandler $handler): self
+    {
+        $this->handler = $handler;
+        return $this;
+    }
 
-	/**
-	 * @var NPCEventHandler
-	 */
-	private $handler = null;
+    public function build(): CustomNPC
+    {
+        if ($this->world instanceof World && $this->position instanceof Vector3 && $this->name !== null && $this->name !== "") {
+            $skin = new Skin(uniqid(), $this->getType()->getImageData(), "", $this->getType()->getGeometryName(), $this->getType()->getGeometry());
+            $nbt = CustomNPC::createBaseNBT($this->position);
+            $skinTag = new CompoundTag("Skin");
+            $skinTag->setTag(new StringTag("Name", $skin->getSkinId()));
+            $skinTag->setTag(new ByteArrayTag("Data", $skin->getSkinData()));
+            $skinTag->setTag(new ByteArrayTag("CapeData", $skin->getCapeData()));
+            $skinTag->setTag(new StringTag("GeometryName", $skin->getGeometryName()));
+            $skinTag->setTag(new ByteArrayTag("GeometryData", $skin->getGeometryData()));
+            $nbt->setTag($skinTag);
+            $npc = new CustomNPC($this->world, $nbt);
+            foreach ($this->tags as $tag) {
+                $npc->addTag($tag);
+            }
+            $npc->setHeader($this->name);
+            $npc->setHandler($this->handler);
+            $npc->update();
+            return $npc;
+        }
 
-	public function setType(CustomType $type): self
-	{
-		$this->type = $type;
-		return $this;
-	}
+        throw new InvalidArgumentException("It seems at least one of the required arguments (World, Position, Header) for your NPC Build is missing");
+    }
 
-	public function addTag(AssignableTag $tag): self
-	{
-		$this->tags[] = $tag;
-		return $this;
-	}
+    public function getName(): string
+    {
+        return $this->name;
+    }
 
-	public function setName(string $name): self
-	{
-		$this->name = $name;
-		return $this;
-	}
+    public function getType(): CustomType
+    {
+        return $this->type;
+    }
 
-	public function setWorld(World $world): self
-	{
-		$this->world = $world;
-		return $this;
-	}
+    public function getPosition(): Vector3
+    {
+        return $this->position;
+    }
 
-	public function setPosition(Vector3 $position): self
-	{
-		$this->position = $position;
-		return $this;
-	}
+    public function getWorld(): World
+    {
+        return $this->world;
+    }
 
-	public function setHandler(NPCEventHandler $handler): self
-	{
-		$this->handler = $handler;
-		return $this;
-	}
+    public function getTags(): array
+    {
+        return $this->tags;
+    }
 
-	public function build(): CustomNPC
-	{
-		if ($this->world instanceof World) {
-			if ($this->position instanceof Vector3) {
-				if ($this->name !== null && $this->name !== "") {
-					$skin = new Skin(uniqid(), $this->getType()->getImageData(), "", $this->getType()->getGeometryName(), $this->getType()->getGeometry());
-					$nbt = CustomNPC::createBaseNBT($this->position);
-					$nbt->setTag(new CompoundTag("Skin"));
-					self::injectSkin($nbt->getCompoundTag("Skin"), $skin);
-					$npc = new CustomNPC($this->world, $nbt);
-					foreach ($this->tags as &$tag) {
-						$npc->addTag($tag);
-					}
-					$npc->setHeader($this->name);
-					$npc->setHandler($this->handler ?? null);
-					$npc->update();
-					return $npc;
-				}
-			}
-		}
-		throw new InvalidArgumentException("It seems at least one of three (World, Position, Header ) required arguments for your NPC Build is missing");
-	}
-
-	public static function injectSkin(CompoundTag $nbt, Skin $skin)
-	{
-		$nbt->setString("Name", $skin->getSkinId());
-		$nbt->setByteArray("Data", $skin->getSkinData());
-		$nbt->setByteArray("CapeData", $skin->getCapeData());
-		$nbt->setString("GeometryName", $skin->getGeometryName());
-		$nbt->setByteArray("GeometryData", $skin->getGeometryData());
-		return $nbt;
-	}
-
-	/**
-	 * @return string
-	 */
-	public function getName(): string
-	{
-		return $this->name;
-	}
-
-	/**
-	 * @return CustomType
-	 */
-	public function getType(): CustomType
-	{
-		return $this->type;
-	}
-
-	/**
-	 * @return Vector3
-	 */
-	public function getPosition(): Vector3
-	{
-		return $this->position;
-	}
-
-	/**
-	 * @return World
-	 */
-	public function getWorld(): world
-	{
-		return $this->world;
-	}
-
-	/**
-	 * @return AssignableTag[]
-	 */
-	public function getTags(): array
-	{
-		return $this->tags;
-	}
-
-	/**
-	 * @param AssignableTag[] $tags
-	 * @return NPCBuilder
-	 */
-	public function setTags(array $tags): self
-	{
-		$this->tags = $tags;
-		return $this;
-	}
+    public function setTags(array $tags): self
+    {
+        $this->tags = $tags;
+        return $this;
+    }
 }
