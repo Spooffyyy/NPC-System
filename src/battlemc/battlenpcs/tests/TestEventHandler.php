79874d1fb5ed17924319f2b0e71400b7c454f9de
CherryBurst103 <145829403+CherryBurst103@users.noreply.github.com>
2023-10-05 05:59:26 -0700

    Update TestEventHandler.php

--- src/battlemc/battlenpcs/tests/TestEventHandler.php
+++ src/battlemc/battlenpcs/tests/TestEventHandler.php
@@ -5,12 +5,16 @@ namespace battlemc\battlenpcs\tests;
 use battlemc\battlenpcs\handler\NPCEventHandler;
 use pocketmine\entity\Entity;
 use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\player\Player;
 
 class TestEventHandler extends NPCEventHandler
 {
-	public function onHit(Entity &$entity, EntityDamageByEntityEvent &$event): bool
-	{
-		$event->getDamager()->sendMessage("lol");
-		return false;
-	}
+    public function onHit(Entity &$entity, EntityDamageByEntityEvent &$event): bool
+    {
+        $damager = $event->getDamager();
+        if ($damager instanceof Player) {
+            $damager->sendMessage("lol");
+        }
+        return false;
+    }
 }
