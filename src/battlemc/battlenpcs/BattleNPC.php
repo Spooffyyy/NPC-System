c33966f01aa54a1b0792eed13a73e7515819d474
CherryBurst103 <145829403+CherryBurst103@users.noreply.github.com>
2023-10-05 04:03:25 -0700

    Update BattleNPC.php

--- src/battlemc/battlenpcs/BattleNPC.php
+++ src/battlemc/battlenpcs/BattleNPC.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace battlemc\battlenpcs;
+
 use battlemc\battlenpcs\caches\TagCache;
 use battlemc\battlenpcs\caches\TypeCache;
 use battlemc\battlenpcs\classes\NPCBuilder;
@@ -10,13 +12,13 @@ use battlemc\battlenpcs\utils\ConfigLoader;
 use pocketmine\entity\Entity;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerChatEvent;
-use pocketmine\Player;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\Server;
 
 class BattleNPC extends PluginBase implements Listener{
 
-	public function onEnable()
+	public function onEnable(): void
 	{
 		$this->getServer()->getPluginManager()->registerEvents($this, $this);
 		if(!file_exists($this->getDataFolder() . "config.yml")){
